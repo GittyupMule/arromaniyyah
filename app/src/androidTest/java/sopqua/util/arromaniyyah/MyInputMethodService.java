278ef6e7a10cd8d1115191cb2dbe224a4684114a
saquil2 <saquil2@illinois.edu>
2018-11-28 11:00:27 -0600

    first keyboard commit

---
+++ app/src/androidTest/java/sopqua/util/arromaniyyah/MyInputMethodService.java
@@ -0,0 +1,82 @@
+package sopqua.util.arromaniyyah;
+
+import android.inputmethodservice;
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+public class MyInputMethodService extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+    public MyInputMethodService() {
+        super();
+    }
+
+    @Override
+    public void onPress(int i) {
+
+    }
+
+    @Override
+    public void onRelease(int i) {
+
+    }
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+        InputConnection inputConnection = getCurrentInputConnection();
+        final int backspaceCode = 67;
+        final int returnCode = 66;
+        switch (primaryCode) {
+            case backspaceCode:
+                CharSequence selectedText = inputConnection.getSelectedText(0);
+                if (TextUtils.isEmpty(selectedText)) {
+                    inputConnection.deleteSurroundingText(1, 0);
+                } else {
+                    inputConnection.commitText("", 1);
+                }
+                break;
+            case returnCode:
+                inputConnection.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                break;
+            default:
+                char code = (char) primaryCode;
+                inputConnection.commitText(String.valueOf(code), 1);
+        }
+
+    }
+    @Override
+    public void onText(CharSequence charSequence) {
+
+    }
+    @Override
+    public void swipeLeft() {
+
+    }
+    @Override
+    public void swipeRight() {
+
+    }
+    @Override
+    public void swipeDown() {
+
+    }
+    @Override
+    public void swipeUp() {
+
+    }
+
+    private KeyboardView keyboardView;
+    private Keyboard keyboard;
+
+    @Override
+    public View onCreateInputView() {
+        keyboardView = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard_view, null);
+        keyboard = new Keyboard(this, R.xml.keys_layout_qwerty);
+        keyboardView.setKeyboard(keyboard);
+        keyboardView.setOnKeyboardActionListener(this);
+        return keyboardView;
+    }
+}
