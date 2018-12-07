fd3f5159e614ae52f3fdb7cc0d75559e6a42d77f
GittyupMule <zuarrie@gmail.com>
2018-12-07 11:44:47 -0600

    Romanization class and logic

--- app/src/main/java/sopqua/util/arromaniyyah/MyInputMethodService.java
+++ app/src/main/java/sopqua/util/arromaniyyah/MyInputMethodService.java
@@ -124,6 +124,343 @@ public final class MyInputMethodService extends InputMethodService implements Ke
         }
     }
 
+    public static RomanizationSchema untypeable = new RomanizationSchema() {
+        @Override
+        public String firstPass(ArabicGrapheme input) {
+            switch (input) {
+                case FATHAH:
+                    return "a";
+                case KASRAH:
+                    return "i";
+                case DAMMAH:
+                    return "u";
+                case HANJARIYYAH:
+                    return "ā";
+                case ALIF_MADDAH:
+                    return "'ā";
+                case FATHAH_TANWIN:
+                    return "aⁿ";
+                case KASRAH_TANWIN:
+                    return "iⁿ";
+                case DAMMAH_TANWIN:
+                    return "uⁿ";
+                case SHADDAH:
+                    return "ɔ";
+                case YA:
+                    return "y";
+                case MAQSURAH:
+                    return "á";
+                case WAW:
+                    return "w";
+                case HA:
+                    return "h";
+                case NUN:
+                    return "n";
+                case MIM:
+                    return "m";
+                case LAM:
+                    return "l";
+                case KAF:
+                    return "k";
+                case QAF:
+                    return "q";
+                case FA:
+                    return "f";
+                case GHAYN:
+                    return "ḡ";
+                case AYN:
+                    return "`";
+                case ZA:
+                    return "ẓ";
+                case TTA:
+                    return "ṭ";
+                case DAD:
+                    return "ḍ";
+                case SAD:
+                    return "ṣ";
+                case SHIN:
+                    return "š";
+                case SIN:
+                    return "s";
+                case ZAY:
+                    return "z";
+                case RA:
+                    return "r";
+                case DHAL:
+                    return "ḏ";
+                case DAL:
+                    return "d";
+                case KHA:
+                    return "ẖ";
+                case HHA:
+                    return "ḥ";
+                case JIM:
+                    return "ǧ";
+                case THA:
+                    return "ṯ";
+                case TA:
+                    return "t";
+                case BA:
+                    return "b";
+                case ALIF:
+                    return "â";
+                case HAMZAH:
+                    return "'";
+                case MARBUTAH:
+                    return "ḧ";
+                case MEDIAL_IY_HAMZAH:
+                    return "iy'";
+                case MEDIAL_AY_HAMZAH:
+                    return "ay'";
+                case AL:
+                    return "aɫ";
+                case MEDIAL_HAMZAH_I:
+                    return "'i";
+                case INITIAL_HAMZAH_I:
+                    return "'i";
+                case INITIAL_HAMZAH_U:
+                    return "'u";
+                case INITIAL_HAMZAH_A:
+                    return "'a";
+                case MEDIAL_HAMZAH_U_ON_YA:
+                    return "'u";
+                case MEDIAL_HAMZAH_A_ON_YA:
+                    return "'a";
+                case MEDIAL_HAMZAH_U_ON_WAW:
+                    return "'u";
+                case MEDIAL_HAMZAH_A_ON_WAW:
+                    return "'a";
+                case MEDIAL_HAMZAH_A_ON_ALIF:
+                    return "'a";
+                case FINAL_HAMZAH_ON_YA:
+                    return "'";
+                case FINAL_HAMZAH_ON_WAW:
+                    return "'";
+                case FINAL_HAMZAH_ABOVE_ALIF:
+                    return "'";
+                case FINAL_HAMZAH_BELOW_ALIF:
+                    return "'";
+                default:
+                    return "";
+            }
+        }
+
+        @Override
+        public String postProcessing(String firstPassInput) {
+            if (firstPassInput.contains("ɫ")) { // Solar letters.
+                if (firstPassInput.contains("ɫn")) {
+                    firstPassInput = firstPassInput.replace("ɫn", "nn");
+                } else if (firstPassInput.contains("ɫẓ")) {
+                    firstPassInput = firstPassInput.replace("ɫẓ", "ẓẓ");
+                } else if (firstPassInput.contains("ɫṭ")) {
+                    firstPassInput = firstPassInput.replace("ɫṭ", "ṭṭ");
+                } else if (firstPassInput.contains("ɫḍ")) {
+                    firstPassInput = firstPassInput.replace("ɫḍ", "ḍḍ");
+                } else if (firstPassInput.contains("ɫṣ")) {
+                    firstPassInput = firstPassInput.replace("ɫṣ", "ṣṣ");
+                } else if (firstPassInput.contains("ɫš")) {
+                    firstPassInput = firstPassInput.replace("ɫš", "šš");
+                } else if (firstPassInput.contains("ɫs")) {
+                    firstPassInput = firstPassInput.replace("ɫs", "ss");
+                } else if (firstPassInput.contains("ɫz")) {
+                    firstPassInput = firstPassInput.replace("ɫz", "zz");
+                } else if (firstPassInput.contains("ɫr")) {
+                    firstPassInput = firstPassInput.replace("ɫr", "rr");
+                } else if (firstPassInput.contains("ɫd")) {
+                    firstPassInput = firstPassInput.replace("ɫd", "dd");
+                } else if (firstPassInput.contains("ɫt")) {
+                    firstPassInput = firstPassInput.replace("ɫt", "tt");
+                } else if (firstPassInput.contains("ɫḏ")) {
+                    firstPassInput = firstPassInput.replace("ɫḏ", "ḏḏ");
+                } else if (firstPassInput.contains("ɫṯ")) {
+                    firstPassInput = firstPassInput.replace("ɫṯ", "ṯṯ");
+                } else {
+                    firstPassInput = firstPassInput.replace("ɫ", "l");
+                }
+            }
+            while (firstPassInput.contains("âa")) {
+                firstPassInput.replace("âa", "a");
+            }
+            while (firstPassInput.contains("aâ")) {
+                firstPassInput.replace("aâ", "â");
+            }
+            while (firstPassInput.contains("ɔ")) {
+                firstPassInput =
+                        firstPassInput.substring(
+                                0,
+                                firstPassInput.indexOf("ɔ")
+                        )
+                        + firstPassInput.substring(
+                                firstPassInput.indexOf("ɔ") + 1,
+                                firstPassInput.indexOf("ɔ") + 2
+                        )
+                        + firstPassInput.substring(
+                                firstPassInput.indexOf("ɔ") + 1
+                        );
+            }
+            return firstPassInput;
+        }
+    };
+
+    public static RomanizationSchema asciiIsOkay = new RomanizationSchema() {
+        @Override
+        public String firstPass(ArabicGrapheme input) {
+            switch (input) {
+                case FATHAH:
+                    return "e";
+                case KASRAH:
+                    return "i";
+                case DAMMAH:
+                    return "o";
+                case HANJARIYYAH:
+                    return "a";
+                case ALIF_MADDAH:
+                    return "'a";
+                case FATHAH_TANWIN:
+                    return "en";
+                case KASRAH_TANWIN:
+                    return "in";
+                case DAMMAH_TANWIN:
+                    return "on";
+                case SHADDAH:
+                    return "ɔ";
+                case YA:
+                    return "y";
+                case MAQSURAH:
+                    return "a";
+                case WAW:
+                    return "w";
+                case HA:
+                    return "h";
+                case NUN:
+                    return "n";
+                case MIM:
+                    return "m";
+                case LAM:
+                    return "l";
+                case KAF:
+                    return "k";
+                case QAF:
+                    return "q";
+                case FA:
+                    return "f";
+                case GHAYN:
+                    return "gh";
+                case AYN:
+                    return "`";
+                case ZA:
+                    return "dh";
+                case TTA:
+                    return "t";
+                case DAD:
+                    return "d";
+                case SAD:
+                    return "s";
+                case SHIN:
+                    return "sh";
+                case SIN:
+                    return "s";
+                case ZAY:
+                    return "z";
+                case RA:
+                    return "r";
+                case DHAL:
+                    return "dh";
+                case DAL:
+                    return "d";
+                case KHA:
+                    return "h";
+                case HHA:
+                    return "h";
+                case JIM:
+                    return "j";
+                case THA:
+                    return "th";
+                case TA:
+                    return "t";
+                case BA:
+                    return "b";
+                case ALIF:
+                    return "a";
+                case HAMZAH:
+                    return "'";
+                case MARBUTAH:
+                    return "h";
+                case MEDIAL_IY_HAMZAH:
+                    return "i'";
+                case MEDIAL_AY_HAMZAH:
+                    return "ay'";
+                case AL:
+                    return "aɫ";
+                case MEDIAL_HAMZAH_I:
+                    return "'i";
+                case INITIAL_HAMZAH_I:
+                    return "'i";
+                case INITIAL_HAMZAH_U:
+                    return "'o";
+                case INITIAL_HAMZAH_A:
+                    return "'e";
+                case MEDIAL_HAMZAH_U_ON_YA:
+                    return "'o";
+                case MEDIAL_HAMZAH_A_ON_YA:
+                    return "'e";
+                case MEDIAL_HAMZAH_U_ON_WAW:
+                    return "'o";
+                case MEDIAL_HAMZAH_A_ON_WAW:
+                    return "'o";
+                case MEDIAL_HAMZAH_A_ON_ALIF:
+                    return "'o";
+                case FINAL_HAMZAH_ON_YA:
+                    return "'";
+                case FINAL_HAMZAH_ON_WAW:
+                    return "'";
+                case FINAL_HAMZAH_ABOVE_ALIF:
+                    return "'";
+                case FINAL_HAMZAH_BELOW_ALIF:
+                    return "'";
+                default:
+                    return "";
+            }
+        }
+
+        @Override
+        public String postProcessing(String firstPassInput) {
+            if (firstPassInput.contains("ɫ")) { // No solar letter logic, al- stays al-.
+                firstPassInput = firstPassInput.replace("ɫ", "l");
+            }
+            while (firstPassInput.contains("ae")) {
+                firstPassInput.replace("ae", "a");
+            }
+            while (firstPassInput.contains("ea")) {
+                firstPassInput.replace("ea", "a");
+            }
+            while (firstPassInput.contains("ey")) {
+                firstPassInput.replace("ey", "ay");
+            }
+            while (firstPassInput.contains("iy")) {
+                firstPassInput.replace("iy", "i");
+            }
+            while (firstPassInput.contains("ow")) {
+                firstPassInput.replace("ow", "u");
+            }
+            while (firstPassInput.contains("ɔ")) {
+                firstPassInput =
+                        firstPassInput.substring(
+                                0,
+                                firstPassInput.indexOf("ɔ")
+                        )
+                                + firstPassInput.substring(
+                                firstPassInput.indexOf("ɔ") + 1,
+                                firstPassInput.indexOf("ɔ") + 2
+                        )
+                                + firstPassInput.substring(
+                                firstPassInput.indexOf("ɔ") + 1
+                        );
+            }
+            return firstPassInput;
+        }
+    };
+
     static String mostLikelyWord(String composingText) {
         return mostLikelyWords(composingText)[0];
     }
---
+++ app/src/main/java/sopqua/util/arromaniyyah/RomanizationSchema.java
@@ -0,0 +1,114 @@
+package sopqua.util.arromaniyyah;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+public abstract class RomanizationSchema {
+
+    public static HashMap<char[], ArabicGrapheme> table;
+    static {
+        table.put(new char[]{'\u0671'}, ArabicGrapheme.ALIF_WASLAH);
+        table.put(new char[]{'\u0670'}, ArabicGrapheme.HANJARIYYAH);
+        table.put(new char[]{'\u0652'}, ArabicGrapheme.SUKUN);
+        table.put(new char[]{'\u0651'}, ArabicGrapheme.SHADDAH);
+        table.put(new char[]{'\u0650'}, ArabicGrapheme.KASRAH);
+        table.put(new char[]{'\u064f'}, ArabicGrapheme.DAMMAH);
+        table.put(new char[]{'\u064e'}, ArabicGrapheme.FATHAH);
+        table.put(new char[]{'\u064d'}, ArabicGrapheme.KASRAH_TANWIN);
+        table.put(new char[]{'\u064c'}, ArabicGrapheme.DAMMAH_TANWIN);
+        table.put(new char[]{'\u064b'}, ArabicGrapheme.FATHAH_TANWIN);
+        table.put(new char[]{'\u064a'}, ArabicGrapheme.YA);
+        table.put(new char[]{'\u0649'}, ArabicGrapheme.MAQSURAH);
+        table.put(new char[]{'\u0648'}, ArabicGrapheme.WAW);
+        table.put(new char[]{'\u0647'}, ArabicGrapheme.HA);
+        table.put(new char[]{'\u0646'}, ArabicGrapheme.NUN);
+        table.put(new char[]{'\u0645'}, ArabicGrapheme.MIM);
+        table.put(new char[]{'\u0644'}, ArabicGrapheme.LAM);
+        table.put(new char[]{'\u0643'}, ArabicGrapheme.KAF);
+        table.put(new char[]{'\u0642'}, ArabicGrapheme.QAF);
+        table.put(new char[]{'\u0641'}, ArabicGrapheme.FA);
+        table.put(new char[]{'\u063A'}, ArabicGrapheme.GHAYN);
+        table.put(new char[]{'\u0639'}, ArabicGrapheme.AYN);
+        table.put(new char[]{'\u0638'}, ArabicGrapheme.ZA);
+        table.put(new char[]{'\u0637'}, ArabicGrapheme.TTA);
+        table.put(new char[]{'\u0636'}, ArabicGrapheme.DAD);
+        table.put(new char[]{'\u0635'}, ArabicGrapheme.SAD);
+        table.put(new char[]{'\u0634'}, ArabicGrapheme.SHIN);
+        table.put(new char[]{'\u0633'}, ArabicGrapheme.SIN);
+        table.put(new char[]{'\u0632'}, ArabicGrapheme.ZAY);
+        table.put(new char[]{'\u0631'}, ArabicGrapheme.RA);
+        table.put(new char[]{'\u0630'}, ArabicGrapheme.DHAL);
+        table.put(new char[]{'\u062f'}, ArabicGrapheme.DAL);
+        table.put(new char[]{'\u062e'}, ArabicGrapheme.KHA);
+        table.put(new char[]{'\u062d'}, ArabicGrapheme.HHA);
+        table.put(new char[]{'\u062c'}, ArabicGrapheme.JIM);
+        table.put(new char[]{'\u062b'}, ArabicGrapheme.THA);
+        table.put(new char[]{'\u062a'}, ArabicGrapheme.TA);
+        table.put(new char[]{'\u0629'}, ArabicGrapheme.MARBUTAH);
+        table.put(new char[]{'\u0628'}, ArabicGrapheme.BA);
+        table.put(new char[]{'\u30f4', '\u0627', '\u064e', '\u0644'}, ArabicGrapheme.AL);
+        table.put(new char[]{'\u0627'}, ArabicGrapheme.ALIF);
+        table.put(new char[]{'\u0622'}, ArabicGrapheme.ALIF_MADDAH);
+        table.put(new char[]{'\u0621'}, ArabicGrapheme.HAMZAH);
+        table.put(new char[]{'\u30f4', '\u0625', '\u0650'}, ArabicGrapheme.INITIAL_HAMZAH_I);
+        table.put(new char[]{'\u30f4', '\u0625', '\u064f'}, ArabicGrapheme.INITIAL_HAMZAH_U);
+        table.put(new char[]{'\u30f4', '\u0625', '\u064e'}, ArabicGrapheme.INITIAL_HAMZAH_A);
+        table.put(new char[]{'\u0650', '\u0626', '\u0652'}, ArabicGrapheme.MEDIAL_IY_HAMZAH);
+        table.put(new char[]{'\u064e', '\u0626', '\u0652'}, ArabicGrapheme.MEDIAL_AY_HAMZAH);
+        table.put(new char[]{'\u0626', '\u0650'}, ArabicGrapheme.MEDIAL_HAMZAH_I);
+        table.put(new char[]{'\u0626', '\u064f'}, ArabicGrapheme.MEDIAL_HAMZAH_U_ON_YA);
+        table.put(new char[]{'\u0626', '\u064e'}, ArabicGrapheme.MEDIAL_HAMZAH_A_ON_YA);
+        table.put(new char[]{'\u0624', '\u064f'}, ArabicGrapheme.MEDIAL_HAMZAH_U_ON_WAW);
+        table.put(new char[]{'\u0624', '\u064e'}, ArabicGrapheme.MEDIAL_HAMZAH_A_ON_WAW);
+        table.put(new char[]{'\u0623', '\u064e'}, ArabicGrapheme.MEDIAL_HAMZAH_A_ON_ALIF);
+        table.put(new char[]{'\u0626', '\u30f4'}, ArabicGrapheme.FINAL_HAMZAH_ON_YA);
+        table.put(new char[]{'\u0625', '\u30f4'}, ArabicGrapheme.FINAL_HAMZAH_BELOW_ALIF);
+        table.put(new char[]{'\u0624', '\u30f4'}, ArabicGrapheme.FINAL_HAMZAH_ON_WAW);
+        table.put(new char[]{'\u0623', '\u30f4'}, ArabicGrapheme.FINAL_HAMZAH_ABOVE_ALIF);
+    }
+
+    public static List<ArabicGrapheme> processWord(String word) {
+        String newWord = '\u30f4' + word + '\u30f4';
+        Set<char[]> keySet = table.keySet();
+        char[][] keyArray = keySet.toArray(new char[][]{});
+        Arrays.sort(keyArray, new Comparator<char[]>() {
+            @Override
+            public int compare(char[] o1, char[] o2) {
+                return Character.compare(o2[0], o1[0]);
+            }
+        });
+        ArrayList<ArabicGrapheme> result = new ArrayList<>();
+        outer: while (newWord.length() > 0) {
+            for (char[] key : table.keySet()) {
+                if (newWord.startsWith(new String(key))) {
+                    result.add(table.get(key));
+                    newWord = newWord.substring(key.length);
+                    continue outer;
+                }
+            }
+            newWord = newWord.substring(1);
+        }
+        return result;
+    }
+
+    public abstract String firstPass(ArabicGrapheme input);
+
+    public String firstPass(List<ArabicGrapheme> input) {
+        StringBuilder result = new StringBuilder();
+        for (ArabicGrapheme a : input) {
+            result.append(firstPass(a));
+        }
+        return result.toString();
+    }
+
+    public abstract String postProcessing(String firstPassInput);
+
+    public String parseWord(String input) {
+        return postProcessing(firstPass(processWord(input)));
+    }
+
+}
