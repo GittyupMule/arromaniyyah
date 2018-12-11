799ea1fa91acdbd02b8390da70f303a7373d7ff4
GittyupMule <GittyupMule@users.noreply.github.com>
2018-12-11 15:40:48 -0600

    Oops, replaceAll takes a regex

--- app/src/main/java/sopqua/util/arromaniyyah/OnegramJSON.java
+++ app/src/main/java/sopqua/util/arromaniyyah/OnegramJSON.java
@@ -1,3 +1,3 @@
 ﻿public final class OnegramJSON {
-public static final String onegrams = (OnegramJSON1.onegrams + OnegramJSON2.onegrams + OnegramJSON3.onegrams + OnegramJSON4.onegrams + OnegramJSON5.onegrams).replaceAll("\u0000", "").replaceAll("\ufffd", "");
+public static final String onegrams = (OnegramJSON1.onegrams + OnegramJSON2.onegrams + OnegramJSON3.onegrams + OnegramJSON4.onegrams + OnegramJSON5.onegrams).replaceAll("\\u0000", "").replaceAll("\\ufffd", "");
 }
