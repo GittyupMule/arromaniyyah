7450c7870f197ea4144f82183d81ceb130cb5219
GittyupMule <zuarrie@gmail.com>
2018-12-11 21:58:28 -0600

    Still not done =(

--- app/src/main/java/sopqua/util/arromaniyyah/OnegramJSON.java
+++ app/src/main/java/sopqua/util/arromaniyyah/OnegramJSON.java
@@ -1,3 +1,5 @@
-﻿public final class OnegramJSON {
-public static final String onegrams = (OnegramJSON1.onegrams + OnegramJSON2.onegrams + OnegramJSON3.onegrams + OnegramJSON4.onegrams + OnegramJSON5.onegrams).replaceAll("\\u0000", "").replaceAll("\\ufffd", "").replaceAll("\\\\u0000", "").replaceAll("\\\\ufffd", "");
+﻿package sopqua.util.arromaniyyah;
+
+public final class OnegramJSON {
+//public static final String onegrams = (OnegramJSON1.onegrams + OnegramJSON2.onegrams + OnegramJSON3.onegrams + OnegramJSON4.onegrams + OnegramJSON5.onegrams).replaceAll("\\u0000", "").replaceAll("\\ufffd", "").replaceAll("\\\\u0000", "").replaceAll("\\\\ufffd", "");
 }
